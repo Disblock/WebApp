14dd41ef8a45c3722655d4b6943978745996fa1b
SaillyKevin <75009579+SaillyKevin@users.noreply.github.com>
2022-04-25 18:52:26 +0200

    Added comments

--- modules/blockly/blockly_xml_to_js.js
+++ modules/blockly/blockly_xml_to_js.js
@@ -26,7 +26,14 @@ module.exports = {
      let args = [server_id];
 
      for(var i=0; i<splittedCode.length; i=i+2){
-       if(splittedCode[i]==undefined || splittedCode[i+1]==undefined){return(1)}//Event and actions must be defined
+       //Loop to check that triggers and code are correctly defined
+
+       if(splittedCode[i]==undefined || splittedCode[i+1]==undefined){
+          //Event and actions must be defined
+          logger.debug("Error in code for "+server_id+" : Event or code is undefined. Cancelling...");
+         return(1);
+       }
+
        if(splittedCode[i].includes("event_") && splittedCode[i]!='' && splittedCode[i+1].replace(/(\r\n|\n|\r)/gm, '')!=''){//If trigger isn't event block, do nothing; If user added an action block without instruction, it will be removed
          //Trigger event defined, code defined, and not just some \n
          splittedCodeToSend.push(splittedCode[i], splittedCode[i+1]);
@@ -34,6 +41,7 @@ module.exports = {
      }
 
      for(var i=0; i<splittedCodeToSend.length; i=i+2){
+       //Loop to generate SQL request
 
        args.push(splittedCodeToSend[i], splittedCodeToSend[i+1]);
        sql = sql + ( (sqlCompleted) ? ',':'' ) + '($1, $'+(i+2)+', $'+(i+3)+')';
