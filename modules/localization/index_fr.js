fa1ee1f21bbdf7716ce5b05c2ec12078324c0fd7
SaillyKevin <75009579+SaillyKevin@users.noreply.github.com>
2022-05-24 02:53:59 +0200

    Text on index page is now in localization folder
    Text is not hard-written anymore, but has been moved in "modules/localization" floder instead. This will help to translate in different languages

--- modules/localization/index_fr.js
+++ modules/localization/index_fr.js
@@ -1,4 +1,41 @@
-//File used to translate index page
+//File used to translate index page in French
 module.exports = {
-  home: "Accueil"
+  home: "Accueil",
+  functionnalities: "Fonctionnalités",
+  invite: "Inviter",
+  premium: "Premium",
+  panel: "Panel",
+  title: "Disblock",
+  subtitle: "Actuellement en Alpha Fermée, ouverture à venir !",
+  goToPanel: "Aller sur le panel",
+  div1Text: "Déjà plus de x blocks !",
+  whyDisblock: "Pourquoi Disblock ?",
+
+  whyDisblock1Title: "Créez vos commandes, vos actions, sans devoir maîtriser le développement",
+  whyDisblock1Text: "Avec Disblock, vous pouvez créer facilement vos propres commandes, et réaliser automatiquement des actions lorqu'un événement se réalise !",
+  whyDisblock1Link: "Voir la liste des fonctionnalités",
+
+  whyDisblock2Title: "Personnalisez Tout",
+  whyDisblock2Text: "Vous pouvez créer des commandes, évènements et tout ce qui vous passe par la tête !",
+  whyDisblock2Link: "Voir la liste des blocs",
+
+  whyDisblock3Title: "Une documentation en ligne",
+  whyDisblock3Text: "Dès que j'aurai du temps pour la faire",
+  whyDisblock3Link: "Voir la documentation",
+
+  whyDisblock4Title: "Un support disponible",
+  whyDisblock4Text: "Sur Discord, faut encore que je le fasse",
+  whyDisblock4Link: "Accéder au serveur",
+
+  div2Text: "Commencer à utiliser Disblock",
+  docsTitle: "Une documentation en constante expansion",
+  docsText: "Besoin d'aide ? Nous avons des guides et une communauté Discord !",
+  iWant: "Je veux...",
+  docsTopic1: "Utiliser un événement ?",
+  docsTopic2: "Créer une commande ?",
+  docsTopic3: "Vérifier les permissions ?",
+  docsAsk: "Demander ?",
+  div3Text: "Inviter sur mon serveur",
+  followUsTwitter: "Suivez-nous sur Twitter !"
+
 }
