f62f1a4dc568502740d7fda37fb0707b198f05a1
LJ5O <75009579+LJ5O@users.noreply.github.com>
2022-11-03 22:22:45 +0100

    Tokens are now encrypted when saved in database
    /!\ breaking changes in database for table users
    We use AES-256 to store tokens. A salt is also saved in database. This salt is regenerated every time an user log in.

--- modules/discord_token_regen.js
+++ modules/discord_token_regen.js
@@ -1,4 +1,9 @@
 const fetch = (...args) => import('node-fetch').then(({default: fetch}) => fetch(...args));
+const aesjs = require('aes-js');//Used to encrypt tokens
+const pbkdf2 = require('pbkdf2');//Used to transform process.env.AES_PASSWORD into a valid password for aes
+
+const { promisify } = require('util');
+const pbkdf2Async = promisify(pbkdf2.pbkdf2).bind(pbkdf2);
 
 module.exports = async function(req, database_pool, logger){
   /* This function is used to regenerate the token of an user, with the savec regen_token. */
@@ -9,9 +14,14 @@ module.exports = async function(req, database_pool, logger){
 
   try{
 
-    let refresh_token = ( await database_pool.query("SELECT refresh_token FROM users WHERE user_id=$1;",[req.session.discord_id]) ).rows[0].refresh_token;
-    if(refresh_token){
+    let sqlResult = ( await database_pool.query("SELECT ENCODE(refresh_token, 'hex') AS hex_refresh_token, salt FROM users WHERE user_id=$1;",[req.session.discord_id]) ).rows[0];
+    if(sqlResult.hex_refresh_token && sqlResult.salt){
       //OK, we got the refresh token
+
+      //We will decrypt the token
+      const key = await pbkdf2Async(process.env.AES_PASSWORD, sqlResult.salt, 18/*iterations*/, 256/8, 'sha512');
+      let aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(1));
+      const refreshToken = aesjs.utils.utf8.fromBytes(aesCtr.decrypt(aesjs.utils.hex.toBytes(sqlResult.hex_refresh_token)));//The encrypted hex is converted to bytes, then decrypted, then converted to String
 
       logger.debug("Sending the refresh token to Discord API...");
 
@@ -21,7 +31,7 @@ module.exports = async function(req, database_pool, logger){
           client_id: process.env.clientId,
           client_secret: process.env.clientSecret,
           grant_type: 'refresh_token',
-          refresh_token: refresh_token
+          refresh_token: refreshToken
         }),
         headers: {
           'Content-Type': 'application/x-www-form-urlencoded',
@@ -32,8 +42,16 @@ module.exports = async function(req, database_pool, logger){
 
       if(oauthData.error==undefined && oauthData.message==undefined){
         //If the token is correctly received
+
+        //We will encrypt it and save it in database
+        aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(1));
+        const encryptedToken = aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(oauthData.access_token)));
+        aesCtr = new aesjs.ModeOfOperation.ctr(key, new aesjs.Counter(1));//We need to redefine, since the counter mode of operation maintains internal state
+        const encryptedRefreshToken = aesjs.utils.hex.fromBytes(aesCtr.encrypt(aesjs.utils.utf8.toBytes(oauthData.refresh_token)));
+
+
         logger.debug("Sending an SQL request to the database : set tokens for user with ID "+req.session.discord_id);
-        await database_pool.query("UPDATE users SET token = $1, refresh_token = $2 WHERE user_id = $3;", [oauthData.access_token, oauthData.refresh_token, req.session.discord_id]);
+        await database_pool.query("UPDATE users SET token = DECODE($1, 'hex'), refresh_token = DECODE($2, 'hex') WHERE user_id = $3;", [encryptedToken, encryptedRefreshToken, req.session.discord_id]);
 
         //Correctly ended, sending the new token
         req.session.token = oauthData.access_token;
