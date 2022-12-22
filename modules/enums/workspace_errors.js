9f1638cec0be6cdfe97fb48cd83f7783035b91a2
LJ5O <75009579+LJ5O@users.noreply.github.com>
2022-12-22 21:15:04 +0100

    Added enum for Workspace errors
    Instead of just sending a global error message, this enum will help us to send a message specific to a problem in editor

---
+++ modules/enums/workspace_errors.js
@@ -0,0 +1,12 @@
+'use-strict';
+module.exports = {
+  error: 1,
+  notLoggedIn: 2,
+  banned: 3,
+  notFound: 4,
+  tooManyBlocks: 5,
+  IncorrectlyPlacedBlocks: 6,
+  tooManyOfABlock: 7,
+  invalidRegex: 8,
+  rateLimitError: 9
+}
