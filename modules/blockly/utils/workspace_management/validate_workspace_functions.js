9a96dbcb0c320ad5a213be59dd0ddf7899be7194
LJ5O <75009579+LJ5O@users.noreply.github.com>
2023-10-24 20:13:42 +0200

    Added a check in  backend for forms
    When a form in used in a command, it should be the only block as the command statements. No more blocks can be added along the form_creator block

--- modules/blockly/utils/workspace_management/validate_workspace_functions.js
+++ modules/blockly/utils/workspace_management/validate_workspace_functions.js
@@ -94,7 +94,11 @@ module.exports = {
             commandBlocks[j].type === "block_slash_command_form_creator"
           ) {
             replyOrFormBlockused = true;
-            break;
+          }
+          console.log(j);
+          if (commandBlocks[j].type === "block_slash_command_form_creator" && j>1){//j>1, as the first block is always the creator/root block from .getDescendants()
+            //NO OK, there is more than just a form_creator defined. User must directly use a form block, not add something else
+            return false;
           }
         }
 
