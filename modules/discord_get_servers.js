08a5431039cf9a64b4e20facab0947a1a7ee7a11
LJ5O <75009579+LJ5O@users.noreply.github.com>
2024-02-01 22:10:10 +0100

    Refactored discord_get_servers.js
    Redis fnctions moved in another file, while the main file is easier to understand. Despite that,, I don't feel like it's perfect, so I may come back here sooner or later.
    Next step is tests for this file

--- modules/discord_get_servers.js
+++ modules/discord_get_servers.js
@@ -2,47 +2,19 @@
 const fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 const discordRegen = require("./discord_token_regen.js");
 const bigInt = require("big-integer"); //Used to check permissions on a server
-
-const { promisify } = require("util");
+const {askRedisForGuilds, saveGuildsInRedis} = require("./redis/redis.js");
 
 module.exports = async function (req, databasePool, logger, redisClient) {
   /* This module is used to get the guilds where an user has an Admin permission. We get all guilds from Discord API, then return only guilds with an Admin access. */
 
-  async function askRedisForGuilds(userId) {
-    try {
-      const getAsync = promisify(redisClient.get).bind(redisClient); /* https://stackoverflow.com/a/63349259/19753521 */
-      const guilds = await getAsync(userId + "guilds");
-
-      if (guilds) {
-        logger.debug("Got cached servers from Redis for user " + userId);
-        return JSON.parse(guilds);
-      } else {
-        return undefined;
-      }
-    } catch (err) {
-      logger.error("Error while getting saved guilds for user " + userId + " in redis : " + err);
-      return undefined;
-    }
-  }
-
-  /* ==================================== */
   //If guilds are cached, get them from Redis and return them.
   //That's one less request to Discord API
-  const cachedGuilds = await askRedisForGuilds(req.session.discord_id);
+  const cachedGuilds = await askRedisForGuilds(redisClient, logger, req.session.discord_id);
   if (cachedGuilds) {
     return cachedGuilds;
   }
 
   /* ==================================== */
-
-  async function saveGuildsInRedis(userId, guilds) {
-    try {
-      await redisClient.set(userId + "guilds", JSON.stringify(guilds), "EX", 120); //Stored for 2 min in Redis
-      logger.debug("Cached guilds in redis for user " + userId);
-    } catch (err) {
-      logger.error("Failed to cache guilds for user " + userId + " in redis : " + err);
-    }
-  }
 
   async function askDiscordForGuilds(token) {
     if (token == undefined) {
@@ -100,48 +72,54 @@ module.exports = async function (req, databasePool, logger, redisClient) {
 
   /* ==================================== */
 
-  const discordResponse = await askDiscordForGuilds(req.session.token);
-  if (discordResponse != undefined && discordResponse != "Token not set") {
-    //OK
-    const guilds = getGuildsWhereAdmin(discordResponse);
-    await saveGuildsInRedis(req.session.discord_id, guilds);
-    return guilds;
-  } else if (discordResponse == "Token not set") {
-    //NOT OK, must regen the token
+  const maxTries = 2;//2, so we can try to get servers, and regen the token is neccessary
 
-    logger.debug(req.session.discord_id + " 's token may have expired, we're trying now to regenerate it.");
+  let counter = 0;
+  do{
+    counter++;
 
-    const newToken = await discordRegen(databasePool, logger, req.session.discord_id);
-    if (newToken == undefined) {
-      //Error, the user has removed the application's access ?
-      logger.warn("Error while getting " + req.session.discord_id + " 's guilds, destroying the session...");
-      req.session.destroy();
-      return [];
-    } else {
-      //OK, let's try again
-      req.session.token = newToken;
-      req.session.save(); //Must save to ensure everything is saved
+    const discordResponse = await askDiscordForGuilds(req.session.token);
+    if (discordResponse != undefined && discordResponse != "Token not set") {
+      //OK
+      const guilds = getGuildsWhereAdmin(discordResponse);
+      await saveGuildsInRedis(redisClient, logger, req.session.discord_id, guilds);
+      return guilds;
 
-      const newDiscordResponse = await askDiscordForGuilds(req.session.token);
-      if (newDiscordResponse != undefined && newDiscordResponse != "Token not set") {
-        const guilds = getGuildsWhereAdmin(newDiscordResponse);
-        saveGuildsInRedis(req.session.discord_id, guilds);
-        return guilds;
-      } else {
-        //Fatal error : user may have removed access to the application
-        logger.info(
-          "Error while getting " +
-            req.session.discord_id +
-            " 's guilds, token regenerated but seems useless, destroying the session..."
-        );
+    } else if (discordResponse == "Token not set") {
+      //NOT OK, must regen the token
+
+      logger.debug(req.session.discord_id + " 's token may have expired, we're trying now to regenerate it.");
+
+      const newToken = await discordRegen(databasePool, logger, req.session.discord_id);
+      if (newToken == undefined) {
+        //Error, the user has removed the application's access ?
+        logger.warn("Error while getting " + req.session.discord_id + " 's guilds, destroying the session...");
         req.session.destroy();
         return [];
+      } else {
+        //OK, let's try again
+        req.session.token = newToken;
+        req.session.save(); //Must save to ensure everything is saved
+
+        continue;
       }
+    } else {
+      //NOT OK, was an error
+      logger.warn("Error while getting " + req.session.discord_id + " 's guilds from Discord, destroying the session...");
+      req.session.destroy();
+      return [];
     }
-  } else {
-    //NOT OK, was an error
-    logger.warn("Error while getting " + req.session.discord_id + " 's guilds from Discord, destroying the session...");
-    req.session.destroy();
-    return [];
-  }
+
+  }while(counter<maxTries);
+
+  //If we arrive here, without a return statement being used, we can assume the token wasn't correctly regenerated
+  //Maybe the user removed our access to his tokens
+
+  logger.info(
+    "Error while getting " +
+      req.session.discord_id +
+      " 's guilds, token regenerated but seems useless, destroying the session..."
+  );
+  req.session.destroy();
+  return [];
 };
---
+++ modules/redis/redis.js
@@ -0,0 +1,32 @@
+'use-strict';
+const { promisify } = require("util");
+
+module.exports = {
+
+  askRedisForGuilds: async function (redisClient, logger, userId) {
+    try {
+      const getAsync = promisify(redisClient.get).bind(redisClient); /* https://stackoverflow.com/a/63349259/19753521 */
+      const guilds = await getAsync(userId + "guilds");
+
+      if (guilds) {
+        logger.debug("Got cached servers from Redis for user " + userId);
+        return JSON.parse(guilds);
+      } else {
+        return undefined;
+      }
+    } catch (err) {
+      logger.error("Error while getting saved guilds for user " + userId + " in redis : " + err);
+      return undefined;
+    }
+  },
+
+  saveGuildsInRedis: async function (redisClient, logger, userId, guilds) {
+    try {
+      await redisClient.set(userId + "guilds", JSON.stringify(guilds), "EX", 120); //Stored for 2 min in Redis
+      logger.debug("Cached guilds in redis for user " + userId);
+    } catch (err) {
+      logger.error("Failed to cache guilds for user " + userId + " in redis : " + err);
+    }
+  }
+
+}
