ba97cddd825b5c1267d0f05361f0ff80ee1057dc
Wesmeer12 <wesmeerwdf@gmail.com>
2024-06-10 16:43:58 +0200

    Create index_nl.js

---
+++ modules/localization/index_nl.js
@@ -0,0 +1,48 @@
+"use strict";
+//File used to translate index page in English
+module.exports = {
+  home: "Start",
+  about: "Over",
+  functionnalities: "Functies",
+  invite: "Voeg toe aan mijn server",
+  premium: "Premium",
+  documentation: "Documentatie",
+  support: "Hulp",
+  panel: "Paneel",
+  title: "Disblock",
+  subtitle: "Maak je eigen acties makkelijk !",
+  connectWith: "Login met",
+  goToPanel: "Ga naar dashboard",
+  div1Text: "Meer dan 130 blokken beschikbaar !",
+  whyDisblock: "Waarom Disblock ?",
+
+  whyDisblock1Title: "Maak je commando's, acties, zonder enige programmeer taal te kennen !",
+  whyDisblock1Text:
+    "Met Disblock, Kan je makkelijk je eigen commando's maken, en automatisch een paar acties uitvoeren wanneer er iets gebeurt !",
+  whyDisblock1Link: "Bekijk de fuctie lijst !",
+
+  whyDisblock2Title: "Pas alles aan",
+  whyDisblock2Text:
+    "Je kan evenementen, commando's en alles dat je wil nodig hebben met een volledige geen-code, blok gebaseerde editor !",
+  whyDisblock2Link: "Zie een paar voorbeelden van wat je kan doen met Disblock",
+
+  whyDisblock3Title: "Een online documentatie",
+  whyDisblock3Text: "Wordt aan gewerkt, leer hier hoe je de bot gebruikt !",
+  whyDisblock3Link: "Toegang online documentatie",
+
+  whyDisblock4Title: "Een beschikbare hulp",
+  whyDisblock4Text: "Op Discord, helpen we je graag ;)",
+  whyDisblock4Link: "Join ons op Discord",
+
+  div2Text:
+    '<a href="https://docs.disblock.xyz/disblock/getting-started" target="_blank" rel="noreferrer noopener">Start</a> to use Disblock',
+  docsTitle: "hulp nodig ?",
+  docsText: "Check onze documentatie en Discord support server !",
+  iWant: "Ik wil ...",
+  docsTopic1: "Maak een evenement ?",
+  docsTopic2: "Maak een Embed bericht ?",
+  docsTopic3: "Support ons ?",
+  docsAsk: "Vraag ?",
+  div3Text: "Nodig bot uit op mijn server",
+  followUsTwitter: "Volg ons op Twitter !",
+};
