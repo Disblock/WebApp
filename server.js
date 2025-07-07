48b95168dc9f483ee94702d88b2ae39460ab1387
Kévin Taccoen <75009579+LJ5O@users.noreply.github.com>
2025-07-07 23:27:17 +0200

    Headers security fix
    Final security fix, big thanks to the security researcher who sent me an email about this !
    
    -----
    
    Dear Security Team,
    
    We found missing HTTP security headers on your domain: disblock.xyz.
    
    Missing Headers:
    • Content-Security-Policy: Helps to prevent XSS and injection attacks.
    • Strict-Transport-Security: Enforces HTTPS connections.
    • Referrer-Policy: Controls referrer information leakage.
    • Permissions-Policy: Restricts access to sensitive features.
    
    Current Headers:
    
    Content-Security-Policy: [MISSING]
    Strict-Transport-Security: [MISSING]
    X-Content-Type-Options: nosniff
    X-Frame-Options: deny
    X-XSS-Protection: 1;mode=block
    Referrer-Policy: [MISSING]
    Permissions-Policy: [MISSING]
    
    
    A full HTTP request and response snapshot is attached.
    
    Regards,

--- server.js
+++ server.js
@@ -307,6 +307,10 @@ app.use(async function (req, res, next) {
   res.setHeader("X-Content-Type-Options", "nosniff");
   res.setHeader("X-Frame-Options", "deny");
   res.setHeader("X-XSS-Protection", "1;mode=block");
+  res.setHeader("Content-Security-Policy", "default-src 'self'; script-src 'self'; object-src 'none';");
+  res.setHeader("Strict-Transport-Security", "max-age=63072000; includeSubDomains; preload");
+  res.setHeader("Referrer-Policy", "no-referrer");
+  res.setHeader("Permissions-Policy", "geolocation=(), microphone=(), camera=()");
 
   //Save user language if not defined
   try {
